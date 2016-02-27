3296f24d6d692adf72607dc3a4eb6fae3672f2e8
qiobingyin <qioa7011210157@163.com>
2016-02-27 08:47:00 +0800

    初始化项目

---
+++ 网易新闻/网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻
+//
+//  Created by bing on 16/2/27.
+//  Copyright © 2016年 bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
